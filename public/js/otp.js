cc2943b1d01ec569f08f08219a32efbda9c5a82e
Thomas Merkel <tm@core.io>
2014-03-10 23:37:08 +0100

    split api and web gui

--- public/js/otp.js
+++ public/js/otp.js
@@ -1,9 +1,10 @@
 $(function() {
 	var api = 'http://127.0.0.1:3000'
 
-	var shareTemplate = Handlebars.compile($('#share-template').html())
+	var shareTemplate   = Handlebars.compile($('#share-template').html())
 	var decryptTemplate = Handlebars.compile($('#decrypt-template').html())
-	var resultTemplate = Handlebars.compile($('#result-template').html())
+	var resultTemplate  = Handlebars.compile($('#result-template').html())
+	var alertTemplate   = Handlebars.compile($('#alert-template').html())
 
 	$('#encrypt').submit(function( event ) {
 		$.post( api, $(this).serialize(), function( data ) {
@@ -12,16 +13,19 @@ $(function() {
 		event.preventDefault();
 	})
 
-	
 	var hash = location.hash.replace('#', '')
 	if(hash) {
 		$('#content').html(decryptTemplate())
 	}
 
 	$('#decrypt').submit(function( event ) {
-		$.post( api + '/' + hash, $(this).serialize(), function( data ) {
-			$('#content').html(resultTemplate(data))
-		}, "json")
+		$.post( api + '/' + hash, $(this).serialize())
+			.done(function( data ) {
+				$('#content').html(resultTemplate(data))
+			}, "json")
+			.fail(function(xhr, textStatus, errorThrown) {
+				$(".alert").alert()
+			})
 		event.preventDefault();
 	})
 })
